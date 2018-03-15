8313cde21029fc624ab3b45795935a0a3eda49b3
Mridul Biswas <mridul@techmancer.com>
2018-03-15 12:36:24 +0530

    Create user and login class

---
+++ classes/Login.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright (c) 2018 Ascentspark Technologies Pvt. Ltd.
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: ascentspark
+ * Date: 15/03/18
+ * Time: 12:32 PM
+ */
+
+namespace classes;
+
+
+class Login
+{
+
+}
---
+++ classes/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ascentspark
+ * Date: 15/03/18
+ * Time: 12:32 PM
+ */
+
+namespace classes;
+
+
+class User
+{
+
+}
